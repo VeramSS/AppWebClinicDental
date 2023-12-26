e65f709babacb045b4e36a21c8265f7abaf8614f
Miguel Vera Sinche <veramiguel526@gmail.com>
2023-12-26 00:07:32 -0500

    Primer commit del proyecto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ComponentsComponent } from './components/components.component';
+import { ServicesComponent } from './services/services.component';
+import { HomeComponent } from './components/home/home.component';
+import { AboutComponent } from './components/about/about.component';
+import { SharedComponent } from './components/shared/shared.component';
+import { NavbarComponent } from './components/shared/navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ComponentsComponent,
+    ServicesComponent,
+    HomeComponent,
+    AboutComponent,
+    SharedComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
